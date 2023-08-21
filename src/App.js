aa019d445043f355774bc68202abd582c3cc1287
AleksaBrains22 <aleksa.jeremic.brains22@gmail.com>
2023-08-22 01:45:24 +0200

    Accordion and componet/pages folder

--- src/App.js
+++ src/App.js
@@ -1,38 +1,25 @@
-import Button from "./components/Button";
-import { GoBell, GoBold, GoDatabase } from "react-icons/go";
+import Accordion from "./components/Accordion";
 
 function App() {
-  return (
-    <div>
-      <div>
-        <Button secondary rounded className="mb-5">
-          <GoBell />
-          Click me!!
-        </Button>
-      </div>
-      <div>
-        <Button warning rounded className="mb-5">
-          <GoDatabase />
-          Buy now !
-        </Button>
-      </div>
-      <div>
-        <Button danger className="mb-5">
-          {" "}
-          <GoBold />
-          See deal!
-        </Button>
-      </div>
-      <div>
-        <Button secondary className="mb-5">
-          Random
-        </Button>
-      </div>
-      <div>
-        <Button success>asd</Button>
-      </div>
-    </div>
-  );
+  const items = [
+    {
+      id: 123,
+      label: "Can I use React on a project?",
+      content: "You can use React on any project you want.",
+    },
+    {
+      id: 12,
+      label: "Can I use Javascript on a project?",
+      content: "You can use Javascript on any project you want.",
+    },
+    {
+      id: 1,
+      label: "Can I use CSS on a project?",
+      content: "You can use CSS on any project you want.",
+    },
+  ];
+
+  return <Accordion items={items} />;
 }
 
 export default App;
---
+++ src/components/Accordion.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
+function Accordion({ items }) {
+  const [expandedIndex, setExpandedIndex] = useState(-1);
+
+  const handleClick = (nextIndex) => {
+    setExpandedIndex((curentExpandedIndex) => {
+      if (curentExpandedIndex === nextIndex) {
+        return -1;
+      } else {
+        return nextIndex;
+      }
+    });
+    // works fine has a bug functional update stile fixes it when we click on the accordion from console 2 times in a row
+    // if (expandedIndex === nextIndex) {
+    //   setExpandedIndex(-1);
+    // } else {
+    //   setExpandedIndex(nextIndex);
+    // }
+  };
+  const renderedItems = items.map((item, index) => {
+    const isExpanded = index === expandedIndex;
+
+    const icon = <span className="text-2xl">{isExpanded ? <GoChevronDown /> : <GoChevronLeft />}</span>;
+    return (
+      <div key={item.id}>
+        <div
+          className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer"
+          onClick={() => handleClick(index)}
+        >
+          {item.label}
+          {icon}
+        </div>
+        {isExpanded && <div className="border-b p-5">{item.content}</div>}
+      </div>
+    );
+  });
+  return <div className="border-x border-t rounded"> {renderedItems}</div>;
+}
+
+export default Accordion;
