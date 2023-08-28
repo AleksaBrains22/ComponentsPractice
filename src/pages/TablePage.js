eef93218637755f5d8a89b024fac299a7190ae0e
AleksaBrains22 <aleksa.jeremic.brains22@gmail.com>
2023-08-28 23:22:58 +0200

    SortableTable

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,5 +1,5 @@
 import Table from "../components/Table";
-
+import SortableTable from "../components/SortableTable";
 function TablePage() {
   const data = [
     {
@@ -32,12 +32,13 @@ function TablePage() {
     {
       label: "Name",
       render: (fruit) => fruit.name,
+      sortValue: (fruit) => fruit.name,
     },
     {
       label: "Color",
       render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div>,
     },
-    { label: "Score", render: (fruit) => fruit.score },
+    { label: "Score", render: (fruit) => fruit.score, sortValue: (fruit) => fruit.score },
   ];
 
   const keyFn = (fruit) => {
@@ -45,7 +46,7 @@ function TablePage() {
   };
   return (
     <div>
-      <Table data={data} config={config} keyFn={keyFn} />
+      <SortableTable data={data} config={config} keyFn={keyFn} />
     </div>
   );
 }
